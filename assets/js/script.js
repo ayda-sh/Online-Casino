63a6f267d120b7ab79fb7d42c4bc8378456e3a64
ayda <aydashamma4@gmail.com>
2025-02-20 09:22:40 -0800

    faq section and footer completed

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-    const resNav = document.getElementById("navbar");
-    if (resNav.className === "navbar") {
-        resNav.className += " responsive";
-    } else {
-        resNav.className = "navbar";
-    }
-  }
